c6a4b41ff665134b8fc37ec8ed6c51065f1b070f
miguelidevelop <miguelidevelop@gmail.com>
2019-12-12 12:18:57 +0000

    Trabajar con controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class HomeController extends Controller
+{
+    public function home() {
+
+        return 'hola mundo';
+    }
+
+
+
+
+}
+
+
+
+
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,28 @@
 <?php
 
 /*
+|    Realizamos una llamada al Controller
+*/
+
+Route::get('/', 'HomeController@home');
+
+
+
+
+
+/*
 |   Route::get('/', function () {
 |    return view('welcome');
 |    });
 
 |
 */
-    Route::get('/', function() {
-        return 'hola mundo';
-    });
+
+/*
+|    Route::get('/', function() {
+|        return 'hola mundo';
+|    });
+*/
 /*
 |   Crear la ruta de una url
 |
@@ -22,18 +35,17 @@
 |   Las rutas pueden ser caoticas a partir de las 20 o 30 paginas, a partir de
 |    ahí es mejor usar Controllers
 */
-
-    Route::get('usuarios/{nombre?}', function($nombre='Esther, Mario, Yolanda, Maria, Nuria, Jose'){
-        return $nombre;
-    })->where('nombre', '[a-zA-Z]+');
-
-    Route::get('usuarios', function(){
-        //Devuelve todos los usuarios
+/*
+|   Route::get('usuarios/{nombre?}', function($nombre='Esther, Mario, Yolanda, Maria, Nuria, Jose'){
+|       return $nombre;
+|   })->where('nombre', '[a-zA-Z]+');
+|
+|        //Devuelve todos los usuarios
     });
 
     Route::post('usuarios', function(){
         //Crea un nuevo usuario
     });
-
+*/
 
 ?>
