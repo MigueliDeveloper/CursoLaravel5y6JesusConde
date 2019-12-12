6ad67e377e27ad7d998f92dfb0bd21467b9a3e3a
miguelidevelop <miguelidevelop@gmail.com>
2019-12-12 12:04:31 +0000

    Curso de Laravel de Jesus Conde Creando primeras rutas

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|   Route::get('/', function () {
+|    return view('welcome');
+|    });
+
+|
+*/
+    Route::get('/', function() {
+        return 'hola mundo';
+    });
+/*
+|   Crear la ruta de una url
+|
+|   Route::get('testing/cualquiera/nuevo', function(){
+|        return 'Aprendiendo rutas';
+|    })
+*/
+
+/*
+|   Las rutas pueden ser caoticas a partir de las 20 o 30 paginas, a partir de
+|    ahí es mejor usar Controllers
+*/
+
+    Route::get('usuarios/{nombre?}', function($nombre='Esther, Mario, Yolanda, Maria, Nuria, Jose'){
+        return $nombre;
+    })->where('nombre', '[a-zA-Z]+');
+
+    Route::get('usuarios', function(){
+        //Devuelve todos los usuarios
+    });
+
+    Route::post('usuarios', function(){
+        //Crea un nuevo usuario
+    });
+
+
+?>
